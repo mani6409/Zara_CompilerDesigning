f13d3e759c3ba1a8404950415a790141cbaa873d
Mani kumar <mani101296409@gmail.com>
2026-03-28 09:30:18 +0530

    Create initial unit test for Parser class

---
+++ src/test/java/com/zara/parser/ParserTest.java
@@ -0,0 +1,17 @@
+// Unit test for Parser class
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParserTest {
+
+    @Test
+    void testParsingValidInput() {
+        // Add test logic here
+    }
+
+    @Test
+    void testParsingInvalidInput() {
+        // Add test logic here
+    }
+}

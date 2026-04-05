b8178db8e5fdec27b65e7aa4039ba8b28d688512
akankshakushwaha2314@gmail.com <akankshakushawa2314@gmail.com>
2026-04-05 17:02:15 +0530

    Fix lexer bug: throw error for unrecognized characters instead of ignoring them

--- src/main/java/com/zara/lexer/Tokenizer.java
+++ src/main/java/com/zara/lexer/Tokenizer.java
@@ -155,8 +155,8 @@ public class Tokenizer {
                     case '<' -> tokens.add(new Token(TokenType.LESS, "<", lineNum));
                     case '=' -> tokens.add(new Token(TokenType.EQUALS, "=", lineNum));
                     case ':' -> tokens.add(new Token(TokenType.COLON, ":", lineNum));
-                    default -> {
-                        /* ignore */ }
+                    default -> throw new RuntimeException(
+                            "Unrecognised character '" + c + "' at line " + lineNum);
                 }
                 i++;
             }
